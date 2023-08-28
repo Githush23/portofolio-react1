5d3976a05f4ac1b3d245f2cf1ec530817f0f1656
Githush23 <fariswidiantoro@gmail.com>
2023-08-28 23:21:49 +0700

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>
+              Hi. I'm Faris Asyraf Widiantoro, nice to meet you. Please take a
+              look around.
+            </p>
+          </div>
+          <div>
+            <p>
+              I'm a fresh graduate and generalist person. Have work experience
+              as an administrative staff and Autocad Drafter in internship.
+              Highly skilled in communication and collaboration, understand web
+              development, have desire to keep learning and keep up with
+              globalization trends, adaptableperson, and able to work under
+              pressure.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+import landingPage from "../assets/project/landing page.jpg";
+import calculator from "../assets/project/calculator.jpg";
+import movieApi from "../assets/project/movieapi.jpg";
+export const data = [
+  {
+    id: 1,
+    name: "React JS Web API",
+    image: movieApi,
+    github: "https://github.com/Githush23/movie-api-react",
+    live: "https://githush23.github.io/movie-api-react/",
+  },
+  {
+    id: 2,
+    name: "React JS Calculator",
+    image: calculator,
+    github: "https://github.com/Githush23/calculator-reactjs",
+    live: "https://Githush23.github.io/calculator-reactjs",
+  },
+  {
+    id: 3,
+    name: "React JS Landing Page",
+    image: landingPage,
+    github: "https://github.com/Githush23/portofolio-web-01",
+    live: "https://faris-portofolio01.netlify.app/",
+  },
+];
